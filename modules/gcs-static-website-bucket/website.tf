3804f79c9835e306675348e4425f0cea51a20a58
Turan Tosun <turantofficial@web.de>
2025-09-04 02:58:56 +0200

    Create website.tf

---
+++ modules/gcs-static-website-bucket/website.tf
@@ -0,0 +1,8 @@
+resource "google_storage_bucket" "bucket" {
+  name               = var.name
+  project            = var.project_id
+  location           = var.location
+  storage_class      = "STANDARD"
+  force_destroy      = true
+  uniform_bucket_level_access = true
+}
