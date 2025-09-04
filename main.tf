2ee372bcfef15100e11f7645c0700fbba2729d66
Turan Tosun <turantofficial@web.de>
2025-09-04 02:56:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,54 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
+      source  = "hashicorp/google"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "google" {
-
-  project = "qwiklabs-gcp-03-9e9da7c2aa16"
-  region  = "us-west1"
-  zone    = "us-west1-c"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
+# VPC
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "tf-vpc"
 }
 
-#terraform init
-#terraform show
+# Firewall
+resource "google_compute_firewall" "tf-firewall" {
+  name    = "tf-firewall"
+  network = google_compute_network.vpc_network.self_link
+
+  direction = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Modules
+module "instances" {
+  source     = "./modules/instances"
+  region     = var.region
+  zone       = var.zone
+  project_id = var.project_id
+}
+
+module "storage" {
+  source     = "./modules/storage"
+  region     = var.region
+  zone       = var.zone
+  project_id = var.project_id
+}
+
+module "gcs-static-website-bucket" {
+  source     = "./modules/gcs-static-website-bucket"
+  name       = var.name
+  project_id = var.project_id
+  location   = "US"
+}
