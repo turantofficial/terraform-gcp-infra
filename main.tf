e125a35db8354b753cbb8d8bfde6dca69a70317f
Turan Tosun <turantofficial@web.de>
2025-09-04 02:53:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,19 @@
-provider "google" {
-  project = "my-gcp-project"
-  region  = "europe-west3"
-}
-
-resource "google_compute_network" "vpc" {
-  name = "turan-vpc"
-}
-
-resource "google_compute_instance" "vm" {
-  name         = "turan-vm"
-  machine_type = "e2-medium"
-  zone         = "europe-west3-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
     }
   }
+}
 
-  network_interface {
-    network = google_compute_network.vpc.name
-    access_config {}
-  }
+provider "google" {
+
+  project = "qwiklabs-gcp-03-9e9da7c2aa16"
+  region  = "us-west1"
+  zone    = "us-west1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
